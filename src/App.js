163762ed32a52525ea7668e044d0dbc6042ff0bc
Sevi <correosevi@gmail.com>
2021-04-05 20:36:31 +0100

    Add Global Styles component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //Pages
+import GlobalStyle from './components/GlobalStyled';
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
