7fc67df82835b34fca597546df936e9753204be3
Sevi <correosevi@gmail.com>
2021-04-05 20:14:58 +0100

    Create the faq section component

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import AboutSection from '../components/AboutSection/Index';
 import ServicesSection from '../components/ServicesSection/Index';
+import FAQ from '../components/FaqSection/Index';
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FAQ />
     </>
   );
 };
