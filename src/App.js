ad1babd1b57b9516944900b6b12a035340ace6e5
Sevi <correosevi@gmail.com>
2021-04-07 14:26:21 +0100

    Create Navigation styled styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //Pages
 import GlobalStyle from './components/GlobalStyled';
 import AboutUs from './pages/AboutUs';
+import Navigation from './components/Navigation/Index';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Navigation />
       <AboutUs />
     </div>
   );
--- src/components/Navigation/Index.js
+++ src/components/Navigation/Index.js
@@ -4,7 +4,9 @@ const Navigation = () => {
   return (
     <StyledNav>
       <h1>
-        <a href="#">Capture</a>
+        <a id="logo" href="#">
+          Capture
+        </a>
       </h1>
       <ul>
         <li>
@@ -21,7 +23,31 @@ const Navigation = () => {
   );
 };
 
-const StyledNav = styled`
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background-color: #282828;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+  #logo {
+    font-size: 1.5rem;
+    font-family: 'Lobster', cursive;
+    font-weight: lighter;
+  }
 `;
 
 export default Navigation;
