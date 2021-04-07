e5b7ad23f0df757cf48020afa326fdf0b8d47a92
Sevi <correosevi@gmail.com>
2021-04-07 11:48:25 +0100

    Create styles for FaqSection

--- src/components/FaqSection/Index.js
+++ src/components/FaqSection/Index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import styled from 'styled-components';
+import { StyledLayout } from '../../styles';
 
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <StyledFaq>
       <h2>
         Any questions <span>FAQ</span>
       </h2>
@@ -15,6 +16,7 @@ const FaqSection = () => {
             sunt!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
@@ -25,6 +27,7 @@ const FaqSection = () => {
             sunt!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different payment methods</h4>
@@ -35,6 +38,7 @@ const FaqSection = () => {
             sunt!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products do you offer</h4>
@@ -45,9 +49,39 @@ const FaqSection = () => {
             sunt!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </StyledFaq>
   );
 };
+
+const StyledFaq = styled(StyledLayout)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
