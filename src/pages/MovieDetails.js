e098fd3a702e54ffac48ee70d10d97c9e8791754
Sevi <correosevi@gmail.com>
2021-04-08 20:27:02 +0100

    Create movie detail page

---
+++ src/pages/MovieDetails.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+
+const MovieDetails = () => {
+  return (
+    <div>
+      <h1>Movie Detail</h1>
+    </div>
+  );
+};
+
+export default MovieDetails;
