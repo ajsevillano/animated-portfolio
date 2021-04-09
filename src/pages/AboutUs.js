eae93d863cf74996fcd212163c85330c4d86d212
Sevi <correosevi@gmail.com>
2021-04-09 15:56:47 +0100

    Add a page animation to About us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,13 +2,17 @@ import AboutSection from '../components/AboutSection/Index';
 import ServicesSection from '../components/ServicesSection/Index';
 import FAQ from '../components/FaqSection/Index';
 
+//Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../utils/animations';
+
 const AboutUs = () => {
   return (
-    <>
+    <motion.div variants={pageAnimation} initial="hidden" animate="show">
       <AboutSection />
       <ServicesSection />
       <FAQ />
-    </>
+    </motion.div>
   );
 };
 
