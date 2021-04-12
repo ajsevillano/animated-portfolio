59d359d6c6bb2eb896974d404401063f351d7002
Sevi <correosevi@gmail.com>
2021-04-12 14:41:49 +0100

    Code cleaning

--- src/utils/useScroll.js
+++ src/utils/useScroll.js
@@ -1,15 +1,10 @@
 //useScroll custom hook
-
 import { useInView } from 'react-intersection-observer';
 import { useAnimation } from 'framer-motion';
 
 export const useScroll = () => {
   const controls = useAnimation();
-  const [element, view] = useInView({ threshold: 0.5 });
-  if (view) {
-    controls.start('show');
-  } else {
-    controls.start('hidden');
-  }
+  const [element, view] = useInView({ threshold: 0.2 });
+  view ? controls.start('show') : controls.start('hidden');
   return [element, controls];
 };
